8f26205a5d12ec127785d085e6faca5b01c1030d
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2023-06-26 18:41:05 -0300

    add: connection with user routes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import express from 'express'
 
+import route from './routes/userRoutes.js'
+
 const app = express()
+
+app.use(express.json())
+app.use("/", route)
 
 app.listen(3000, () => console.log("Server is running in port 3000..."))
