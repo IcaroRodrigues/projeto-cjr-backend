0f3451da69cf4b8c3bb7886e79bd89008790bcc2
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2023-07-07 19:45:01 -0300

    create: user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express'
+
 import { createUserController } from '../controllers/createUserController.js';
 import { findAllUserController } from '../controllers/findAllUsersController.js';
+import { deleteUserController } from '../controllers/deleteUserController.js';
+import { findUserController } from '../controllers/findUserController.js';
+import { updateUserController } from '../controllers/updateUserController.js';
 
 const route = Router()
 
-route.post("/user", createUserController)
 route.get("/users", findAllUserController)
-
+route.get("/user/:id", findUserController)
+route.post("/user", createUserController)
+route.put("/user/:id", updateUserController)
+route.delete("/user/:id", deleteUserController)
 
 export default route;
